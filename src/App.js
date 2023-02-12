323534e476fff5f7156bf75bf5634d658ad25c4f
vishnujat922 <vishnujat922@gmail.com>
2023-02-12 18:48:05 +0530

    class assignment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+//1.Import Area
 import logo from './logo.svg';
 import './App.css';
 
+//1.1 Import{ Some Named Import} from somelocation/somelibrary;
+
+//1.2 Import someDefaultImport from somelocation/ somelibrary;
+import CompA from './Components/CompA';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CompA address="From Neemuch"/>
+      <CompA address="From Manasa near Village Pawati"/>
     </div>
   );
 }
---
+++ src/Components/CompA.js
@@ -0,0 +1,18 @@
+//1. Import Area
+
+
+//2. Function Definition Area
+function CompA(props){
+    let name="Vishnu Kumar";//Local variable
+    //Every function Return Somrthing
+    return(
+        <h1>Hello Everyone I'm {name}{props.address},</h1>
+    )
+}
+
+
+//3. Export Area
+//You can Export Anything
+//3.1 Default Export Area
+export default CompA;
+//3.2 Named Export Area 
